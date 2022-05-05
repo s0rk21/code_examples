bd15a59fc52adbb889787d56ab98fa23e20cefb5
Артем Крысов <s0rk199@gmail.com>
2022-05-05 19:10:17 +0300

    a several new succeded tasks

--- codewars/CodeWars.py
+++ codewars/CodeWars.py
@@ -29,3 +29,55 @@ def sort_array(source_array):
 
 def duplicate_encode(word):
     return ''.join(['(' if word.lower().count(letter) == 1 else ')' for letter in word.lower()])
+
+
+# Task 3
+# Complete the solution so that it splits the string into pairs of two characters. If the string
+# contains an odd number of characters then it should replace the missing second character of the
+# final pair with an underscore ('_').
+# * 'abc' =>  ['ab', 'c_']
+# * 'abcdef' => ['ab', 'cd', 'ef']
+
+def solution(s):
+    res = []
+    if len(s) % 2 == 1:
+        s = s[:] + '_'
+    for offset, i in enumerate(s):
+        if offset % 2 == 1:
+            res.append(s[offset - 1] + s[offset])
+    return res
+
+
+# Task 4
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+# Leave punctuation marks untouched.
+# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+# pig_it('Hello world !')     # elloHay orldway !
+
+def pig_it(text):
+    text = text.split()
+    for offset, word in enumerate(text):
+        if word not in '!,.:;?':
+            text[offset] = word[1:] + word[0] + 'ay'
+    return ' '.join(text)
+
+
+# Task 5
+# Define a function that takes one integer argument and returns logical value true or false
+# depending on if the integer is a prime.
+# Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no
+# positive divisors other than 1 and itself.
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, 10):
+        if num % i == 0:
+            if num != i:
+                return False
+    if str(num)[-1] in ('3', '1', '9'):
+        for i in range(2, 10000):
+            if num % i == 0:
+                if num != i:
+                    return False
+    return True
