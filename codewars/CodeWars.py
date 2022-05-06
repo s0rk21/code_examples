f694b7157e49d3aed245076f4c55a752c5248bc3
Артем Крысов <s0rk199@gmail.com>
2022-05-06 15:05:36 +0300

    additional 6th Task

--- codewars/CodeWars.py
+++ codewars/CodeWars.py
@@ -81,3 +81,13 @@ def is_prime(num):
                 if num != i:
                     return False
     return True
+
+# Task 6
+# # # Write a function that when given a URL as a string, parses out just the domain name and returns
+# # # it as a string. For example:
+# * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+# * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+# * url = "https://www.cnet.com"                -> domain name = cnet"
+
+def domain_name(url):
+    return url.split('//')[-1].split('www.')[-1].split('.')[0]
