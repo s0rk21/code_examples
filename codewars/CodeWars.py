702917b44f6482fe2769b82c0543ce583b1d42fc
Артем Крысов <s0rk199@gmail.com>
2022-05-07 14:02:10 +0300

    new one

--- codewars/CodeWars.py
+++ codewars/CodeWars.py
@@ -82,6 +82,7 @@ def is_prime(num):
                     return False
     return True
 
+
 # Task 6
 # # # Write a function that when given a URL as a string, parses out just the domain name and returns
 # # # it as a string. For example:
@@ -91,3 +92,28 @@ def is_prime(num):
 
 def domain_name(url):
     return url.split('//')[-1].split('www.')[-1].split('.')[0]
+
+
+# Task 7
+# Step 1: Create a function called encode() to replace all the lowercase vowels in a given string
+# with numbers according to the following pattern:
+# a -> 1
+# e -> 2
+# i -> 3
+# o -> 4
+# u -> 5
+# For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels
+# in this kata.
+# Step 2: Now create a function called decode() to turn the numbers back into vowels according to the
+# same pattern shown above.
+# For example, decode("h3 th2r2") would return "hi there".
+# For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+def encode(st):
+    VOWELS = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+    return ''.join([str(VOWELS[i]) if i in VOWELS else i for i in st])
+
+
+def decode(st):
+    VOWELS = {'1': 'a', '2': 'e', '3': 'i', '4': 'o', '5': 'u'}
+    return ''.join([VOWELS[i] if i in VOWELS else i for i in st])
